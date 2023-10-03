611662611137b201259298abafbe47ef573df9bd
Luca120420 <luca.lombardi120@gmail.com>
2023-10-03 14:36:54 +0200

    Pulizia stringa
    Aggiunta la pulizia della stringa di coordinate della box

--- cat-segmentation-YOLO/main.py
+++ cat-segmentation-YOLO/main.py
@@ -5,9 +5,11 @@ model = YOLO("YOLOv8x.pt")  # load a pretrained model (recommended for training)
 
 # Use the model
 
-results = model(source="dfa68c08-d00c-4d79-8d40-65f2334439d5.jpg", show=True, conf=0.6, classes=15) # predict on an image
+results = model(source="cat-segmentation-YOLO\maine-coon-cat-photography-robert-sijka-67-57ad952ba9cac__880.jpg", show=False, conf=0.6, classes=15) # predict on an image
 
 for result in results:
     with open("coordinategatto1.txt","w") as f:
         coordinate = (str(result.boxes.xywh[0]))
+        coordinate = coordinate.replace("tensor(", "").replace(")", "")
+        coordinate = coordinate.strip('[]')
         f.write(coordinate)
