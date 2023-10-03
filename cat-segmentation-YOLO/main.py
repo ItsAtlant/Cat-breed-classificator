cbc56aa2ef0c40c13283dacd45f9a5fdd061eb33
Davide Soltys <Davide02072003@gmail.com>
2023-10-03 15:26:16 +0200

    Cat yolo segmentation funzionante
    Avendo una cartella di immagini di gatti, ti restituisce una cartella con le coordinate di essi

--- cat-segmentation-YOLO/main.py
+++ cat-segmentation-YOLO/main.py
@@ -1,13 +1,31 @@
 from ultralytics import YOLO
+import os
+
+
+if not os.path.isfile("YOLOv8x.pt"):
+    #TODO fare il download leggendo il file yolodownload.txt
+    ...
 
 # Load a model
 model = YOLO("YOLOv8x.pt")  # load a pretrained model (recommended for training)
 
-# Use the model
 
-results = model(source="dfa68c08-d00c-4d79-8d40-65f2334439d5.jpg", show=True, conf=0.6, classes=15) # predict on an image
+#TODO mettere i commenti e se si riesce pulire un po il codice
+for (root, dirs, files) in os.walk('fotogatti', topdown=True):
 
-for result in results:
-    with open("coordinategatto1.txt","w") as f:
-        coordinate = (str(result.boxes.xywh[0]))
-        f.write(coordinate)
+    root_controllo = root.split("_")[-1]
+    if root_controllo != "coordinate":
+        for immagine in files:
+            if not os.path.isdir(f"{root}_coordinate"):
+                os.mkdir(f"{root}_coordinate")
+
+            results = model(source=f"{root}/{immagine}", show=False,
+                            conf=0.6, classes=15)  # predict on an image
+
+            for result in results:
+                immagine = immagine.split(".")[0]
+                with open(f"{root}_coordinate/{immagine}.txt", "w") as f:
+                    coordinate = (str(result.boxes.xywh[0]))
+                    coordinate = coordinate.replace("tensor(", "").replace(")", "")
+                    coordinate = coordinate.strip('[]')
+                    f.write(coordinate)
